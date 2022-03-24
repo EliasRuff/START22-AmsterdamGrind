0f2ab7f686e31cf0a4273f9f27a6da0dbaa51f38
Simon Boehi <boehis@student.ethz.ch>
2022-03-24 18:16:37 +0100

    users

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from get_user_activity import *
 uid = 'D0E1963A7D0C8AC343AE79A39D8F5CBE'
 activities = pd.read_json('./activities.json')
 pandaMove = pd.read_json("./movedata.json")
+users = pd.read_json("./users.json")
 
 init(activities,pandaMove)
+for user in users:
+  
 print(getUserBelonging(uid))
 print(get_user_activity(activities, uid))
